d0704e96547effa2797740e4bdde1710443ea43c
NeilRow56 <neilrowland56@gmail.com>
2024-07-24 16:48:02 +0100

    Register action completed and working - prior to updated validation schema

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,12 +2,44 @@
 
 import { RegisterSchema } from "@/schemas/auth";
 import * as z from "zod";
+import bcryptjs from "bcryptjs";
+import db from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
     return { error: "Invalid fields" };
   }
+
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcryptjs.hash(password, 12);
+
+  //check if user already exists
+
+  const existingUser = await db.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return {
+      data: null,
+      error: "This email is already in use",
+      status: 409,
+    };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO Send verification email
 
   return { success: "Registration successful!" };
 };
